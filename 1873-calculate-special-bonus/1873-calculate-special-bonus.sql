bba64442bcd6dee1af21c6f21f10e4aa39ff61f2
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-05-08 15:00:37 +0200

    Time: 789 ms (15.80%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-
+/*
 SELECT E.employee_id,
 CASE 
 WHEN E.employee_id%2 <>0 AND LEFT(E.name,1)<>'M'
@@ -9,3 +9,11 @@ ELSE 0
 END AS bonus
 FROM Employees E
  
+*/
+SELECT E.employee_id,
+CASE 
+WHEN E.employee_id%2 <>0 AND E.name NOT LIKE 'M%'
+THEN E.salary
+ELSE 0
+END AS bonus
+FROM Employees E
