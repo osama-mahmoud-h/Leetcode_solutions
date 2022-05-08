10bb04694f0fdeebc48b5a9368337fe18d2e9f2e
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-05-08 14:56:19 +0200

    Time: 478 ms (91.75%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+SELECT E.employee_id,
+CASE 
+WHEN E.employee_id%2 <>0 AND LEFT(E.name,1)<>'M'
+THEN E.salary
+ELSE 0
+END AS bonus
+FROM Employees E
+ 
