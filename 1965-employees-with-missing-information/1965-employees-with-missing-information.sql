aa10bed40954a35211a38ba7df374e709b581a56
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-05-09 14:32:56 +0200

    Time: 440 ms (90.12%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+
+SELECT E.employee_id
+FROM Employees E LEFT JOIN Salaries S
+ON E.employee_id = S.employee_id
+WHERE E.name IS NULL OR S.salary IS NULL 
+UNION 
+SELECT S.employee_id
+FROM Salaries S LEFT JOIN  Employees E 
+ON E.employee_id = S.employee_id
+WHERE E.name IS NULL OR S.salary IS NULL
+ORDER BY employee_id
+/*
+WHERE
+ORDER BY employee_id ASC*/
