7756dcb995651a56ea572a471ab6f89dfc2e61f9
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-09-21 07:57:14 +0200

    Time: 16 ms (52.24%), Space: 9.9 MB (93.83%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+       unsigned int all=0;
+        for(auto i:nums){
+            all^=i;
+        }
+        cout<<all;
+    all&=-all;
+        int f=0;
+        int l=0;
+        
+        for(int i:nums){
+           if((all&i)==all){
+               f^=i;
+           }else{
+               l^=i;
+           }
+        }
+        
+        return vector<int>{f,l};
+    }
+};
+
+/*
+
+101
+011
+110
+
+010
+101
+
+*/
