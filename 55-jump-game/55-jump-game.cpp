f1451fbd379d1f4a60a10d6632d636abb038f7ff
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-09-27 08:06:31 +0200

    Time: 2623 ms (5.01%), Space: 65.6 MB (5.41%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    unordered_map<int,bool>memo;
+    bool helper(vector<int>&nums,int i){
+        if(memo.find(i)!=memo.end()){
+            return memo[i];
+        }
+        if(i>=nums.size()-1){
+            return 1;
+        }
+        if(nums[i]==0){
+            return 0;
+        }
+        int choices = nums[i];
+        for(int j=choices+i;j>=i+1;j--){
+            if(helper(nums,j)){
+                memo[j]=1;
+                return 1;
+            }
+        }
+        memo[i]=0;
+        return 0;
+    }
+    bool canJump(vector<int>& nums) {
+        return helper(nums,0);
+    }
+};
