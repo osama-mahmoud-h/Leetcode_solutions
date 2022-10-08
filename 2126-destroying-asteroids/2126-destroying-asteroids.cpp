c052f2245df139d006982c4a218ead9790fb5b1b
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-10-08 06:34:42 +0200

    Time: 446 ms (44.95%), Space: 102.7 MB (77.33%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-                sort(asteroids.begin(),asteroids.end());
-    long long int sum=mass;
+        
+    sort(asteroids.begin(),asteroids.end());
     for (int i = 0; i <asteroids.size() ; ++i) {
-        if(sum>=asteroids[i]){
-            sum+=asteroids[i];
+        if(mass>=asteroids[i]||mass>=1e5){
+            mass+=mass>=1e5?0:asteroids[i];
         } else{
             return 0;
         }
