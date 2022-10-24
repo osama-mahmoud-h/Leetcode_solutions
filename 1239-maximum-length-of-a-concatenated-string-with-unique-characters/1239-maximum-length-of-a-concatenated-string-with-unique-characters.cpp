06b9dc51822fc9f64857cb8fa5e170d5bd554e3d
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-10-24 10:24:02 +0200

    Time: 897 ms (12.03%), Space: 206.8 MB (12.82%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,47 +1,29 @@
 class Solution {
-public:
-   bool containDuplicate(vector<string>&str,string& s ){
-    vector<int>map(26,0);
-    for(auto st:str){
-        for(char ch:st) {
-            map[ch - 'a']++;
-        }
-    }
-
-    for(auto ch:s){
-        map[ch-'a']++;
-        if(map[ch-'a']>1){
-            return 1;
-        }
-    }
-    return 0;
-}
-bool containDuplicate(string s){
+public :
+    bool isUnique(string s){
     unordered_set<char>set;
     for(auto c:s){
         if(set.find(c)!=set.end()){
-            return 1;
+            return 0;
         }
         set.insert(c);
     }
-    return 0;
+    return 1;
 }
-int backtrack(vector<string>tmp ,vector<string>& arr,int idx){
+int backtrack(string tmp ,vector<string>& arr,int idx){
     if(idx>=arr.size()){
         return 0;
     }
     int pick =0,leave=0;
-    if(!containDuplicate(arr[idx]) && !containDuplicate(tmp,arr[idx]) ){
-        tmp.push_back(arr[idx]);
-        pick = arr[idx].length() + backtrack(tmp,arr,idx+1);
-        tmp.pop_back();
+    if(isUnique(arr[idx]) && isUnique(tmp+arr[idx]) ){
+        pick = arr[idx].length() + backtrack(tmp+arr[idx],arr,idx+1);
     }
      leave = backtrack(tmp,arr,idx+1);
     return max(pick,leave);
 }
 
 int maxLength(vector<string>& arr) {
-    vector<string> tmp;
+    string tmp = "";
     return backtrack(tmp,arr,0);
 }
 };
