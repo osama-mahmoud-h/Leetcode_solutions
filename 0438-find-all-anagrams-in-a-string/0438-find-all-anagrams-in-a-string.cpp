da03efcdfd4456f3ce5e96aa8d136f9f0db1330a
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-11-26 07:16:52 +0200

    Time: 37 ms (45.09%), Space: 8.5 MB (98.87%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        
         vector<int> ans;
         vector<int>p_map(26,0);
-        for(auto i:p){
-            p_map[i-'a']++;
-        }
-        
-        
-
+        vector<int>sub_map(26,0);
         int n=s.length();
         int m=p.length();
         
-        for(int i = 0; i<=n-m; i++){
-            string sub = s.substr(i,m);
-            vector<int>sub_map(26,0);
-            for(auto i:sub){
-                sub_map[i-'a']++;
-            }
-            
+        if(m>n)return ans;
+        
+        for(auto i:p){
+           
+        }
+        for(int i=0;i<m;i++){
+            p_map[p[i]-'a']++;
+            sub_map[s[i]-'a']++;
+        }
+
+     
+        for(int i = m; i<n; i++){
+             
             bool flag=0;
-            for(int i=0;i<26;i++){
-                if(sub_map[i]!=p_map[i]){flag=1;break;};
+            for(int j=0;j<26;j++){
+                if(sub_map[j]!=p_map[j]){flag=1;break;};
             }
+            sub_map[s[i-m]-'a']--;
+            sub_map[s[i]-'a']++;
             if(flag){
                 continue;
             }
-            
-            ans.push_back(i);
+            ans.push_back(i-m);
             
         }
+
+          bool flag=0;
+            for(int j=0;j<26;j++){
+                if(sub_map[j]!=p_map[j]){flag=1;break;};
+            }
+         
+            if(!flag){
+             ans.push_back(n-m);
+            }
+             
         
             return ans;
     }
