42ad73c12e86409efb3eef85438170823fbf0f48
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2022-12-21 19:05:31 +0200

    Time: 9 ms (59.29%), Space: 9.6 MB (69.33%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size(); 
-        //case 1 [4] missing =4-(0+1)=3
-        if((arr[0]-1)>=k){
-            return k;
-        }
-        //case 2 if((arr[n-1]-n)<k) -> missed= k+ar[n-1]-n
-        if((arr[n-1]-n)<k){
-            return k-(arr[n-1]-n)+arr[n-1];
-        }
+    int findKthPositive(vector<int>& A, int k) {
         
-       // third case between 2 numbers
-        //binary search
-        int l=0,h=n-1;
-        while(l<h){
-            int mid = (l+h)/2;
-            if((arr[mid]-mid-1)<k){
-                l=mid+1;
-            }else{
-                h=mid;
-            }
+    int l = 0, r = A.size(), m;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (A[m] - 1 - m < k)
+                l = m + 1;
+            else
+                r = m;
         }
-        // if((arr[l]-(l+1))>k){
-        //     l--;
-        // }else{
-        //     return k-(arr[l]-(l+1))+arr[l];
-        // }
-      //  cout<<l<<"\n";
-        
-        return k-(arr[l]-l)+arr[l];
+        //cout<<A[l]<<"\n";
+        return l + k;
     }
 };
