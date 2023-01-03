6bf34a560189963c714114f74652c5bad9f1c0a6
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-01-03 08:10:59 +0200

    Time: 6 ms (81.63%), Space: 12.3 MB (71.01%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+   int search_helper(vector<int>& nums,int l,int h){
+    int n = nums.size();
+    if((h-l)==1){
+        return nums[l];
+    }
+    if(nums[h-1]>nums[l]){
+        return nums[l];
+    }
+    int mid = (l+h)/2;
+    return min(search_helper(nums,l,mid), search_helper(nums,mid,h));
+}
+int findMin(vector<int>& nums) {
+    int n =nums.size();
+    return search_helper(nums, 0,n);
+}
+};
