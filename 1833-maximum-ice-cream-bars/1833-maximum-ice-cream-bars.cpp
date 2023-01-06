e0d5f889b63d4bd30942e4c74b32b7327164740f
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-01-06 06:27:19 +0200

    Time: 217 ms (61.14%), Space: 76.5 MB (32.82%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,17 @@
+class Solution{
+private:
+
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int ans=0;
+        for (auto i:costs) {
+            if(coins>=i){
+                ans++;
+                coins-=i;
+            } else{ break;}
+        }
+        return ans;
+    }
+
+};
