cdc0ba5b3e35890232cf17709f67985c7b46e858
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-01-08 07:28:00 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (45.36%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -0,0 +1,38 @@
+#define ll long long int
+class Solution{
+private:
+   ll sum_squence(int from,int to){
+       from = max(0,from);
+       to = max(0,to);
+       ll up = (ll)to*(to+1)/2;
+       ll low = (ll)from*(from+1)/2;
+       return up-low;
+   }
+public:
+    int maxValue(int n, int index, int maxSum) {
+    int l=0,h=1e9+2;
+    int ans=0;
+        while (l<h){
+           int mid = (l+h)/2;
+           int up_empty = n-index-1;
+           int low_empty = index;
+           ll  sum =sum_squence(mid-1-up_empty,mid-1) +
+                    sum_squence(mid-1-low_empty,mid-1)+
+                    mid;
+            
+            sum += (up_empty-(mid-1))>0?(up_empty-(mid-1)):0;
+            sum += (low_empty-(mid-1))>0?(low_empty-(mid-1)):0;
+          
+           // cout<<mid<<" , "<<sum<<"\n";
+           // cout<<"up:"<<up_empty<<" mid-1: "<<mid-1<<"\n";
+           if(sum<=maxSum){
+               ans = mid;
+               l=mid+1;
+           } else{
+               h=mid;
+           }
+        }
+        //cout<<l<<"\n";
+        return ans;
+    }
+};
