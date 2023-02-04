4cbda95956e683db41093ff396fc4ca52847f76f
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-02-04 13:32:50 +0200

    Time: 5 ms (83.79%), Space: 7.3 MB (45.48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        sort(s1.begin(),s1.end());
         
-        int m=s1.length() , n = s2.length();
-       // int str = s2.substr(0,h);
-        for(int i=0;i<n-m+1;i++){
-            string sub = s2.substr(i,m);
-            sort(sub.begin(),sub.end());
-            if(sub==s1){return 1;}
+     int m=s1.length() , n = s2.length();
+        
+        if(m>n){return 0;}
+
+      //  sort(s1.begin(),s1.end());
+        vector<int>map1(26,0) ,map2(26,0);
+        for(auto c:s1){
+            map1[c-'a']++;
         }
+       
+        for(int i=0;i<m;i++){
+            map2[s2[i]-'a']++;
+        }
+        
+        for(int i=m;i<n;i++){
+            bool flag = 1;
+           for(int i=0;i<26;i++){
+               if(map1[i]!=map2[i]){flag=0;break;}
+           }
+            if(flag)
+                return 1;
+           map2[s2[i]-'a']++;
+           map2[s2[i-m]-'a']--;
+        }
+        
+           bool flag = 1;
+           for(int i=0;i<26;i++){
+               if(map1[i]!=map2[i]){flag=0;break;}
+           }
+            if(flag)
+                return 1;
         
         return 0;
         
