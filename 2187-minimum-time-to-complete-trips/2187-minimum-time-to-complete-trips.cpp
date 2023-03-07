7dd1e346c3ec44a29de54247d8ab4b2462d83ba2
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-03-07 06:22:56 +0200

    Time: 341 ms (73.55%), Space: 94.5 MB (76.55%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,31 @@
+#define ll long long 
+
+class Solution {
+private:
+    ll get_total_trips(vector<int>&time,ll t){
+        ll sum = 0;
+        for (const auto &item: time){
+            sum += t/item;
+           // cout<<sum<<"\n";
+        }
+        return sum;
+    }
+public:
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        ll n = time.size(), l = 1, r = 1LL * *max_element(time.begin(), time.end()) * totalTrips;
+        ll sum = 0, ans=-1;
+        cout<<n<<"\n";
+        while (l<r){
+            ll m = (l+r)/2;
+            ll total_trips = get_total_trips(time,m);
+            if(total_trips >= totalTrips){
+                r=m;
+            } else{
+                l=m+1;
+
+            }
+        }
+        
+        return l;
+    }
+};
