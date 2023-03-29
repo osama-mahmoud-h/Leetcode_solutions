86341c17ea26196bbefeb96f0180b32f61590e35
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-03-29 06:02:31 +0200

    Time: 4 ms (88.09%), Space: 8 MB (89.59%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,23 +1,26 @@
+
+
 class Solution {
-    vector<vector<int>>memo;
-private:
-    int helper(int idx ,int coff,vector<int>&ar){
-        int n = ar.size();
-        if(idx>=n){return 0;}
-        if(memo[idx][coff]!=-1){
-            return memo[idx][coff];
-        }
-        int pick = coff * ar[idx]+ helper(idx+1,coff+1,ar);
-        int leave = helper(idx+1,coff,ar);
-        
-        memo[idx][coff] = max(pick,leave);
-        return max(pick,leave);
-    }
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
-        memo = vector<vector<int>>(n,vector<int>(n+1,-1));
         sort(satisfaction.begin(),satisfaction.end());
-        return helper(0,1,satisfaction);
+        int sum = 0;
+        int j=n;
+        for (int i = n-1; i >=0 ; --i) {
+            sum+=satisfaction[i];
+            if(sum>=0){
+                j--;
+            }else{
+                break;
+            }
+        }
+        int i=1;
+        sum = 0;
+        for ( ; j <n ; ) {
+            sum+= (i++) * satisfaction[j++];
+        }
+        
+        return sum;
     }
 };
