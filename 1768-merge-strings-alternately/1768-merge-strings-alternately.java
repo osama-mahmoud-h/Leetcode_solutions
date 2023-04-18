f617a02e807f53753b25b9ff4fad9ea584631487
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-04-18 06:56:02 +0200

    Time: 0 ms (100.00%), Space: 41.1 MB (48.65%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+             StringBuilder sb =new StringBuilder () ;
+        int i=0,j=0;
+        int n=word1.length();
+        int m=word2.length();
+        while(i<n||j<m){
+            if(i<n)
+                sb.append(word1.charAt(i));
+             if(j<m)
+                sb.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        return sb.toString();
+    }
+}
