c1398e7cad238a2e305111445a2661453db83347
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-04-22 12:57:11 +0200

    Time: 1547 ms (9.42%), Space: 292.3 MB (10.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,35 +1,28 @@
-class Solution {
-public:
-    vector<int> memo;
-    
-    int helper(vector<int>& nums,int prevVal,int curIdx){
-       if(curIdx>=nums.size()){
-          return 0;
-        }
-        
-    int pick = 0, leave = 0;
-        
-        
-    if(nums[curIdx]>prevVal){
-        if(memo[curIdx]==-1){
-             memo[curIdx] = 1 + helper(nums,nums[curIdx],curIdx+1);
-        }
-        pick = memo[curIdx];
-    }
-        
-    leave = helper(nums,prevVal,curIdx+1);
 
-    return max(pick,leave);
-}
-int lengthOfLIS(vector<int>& nums) {
-    int n= nums.size();
-    int OO = 1e5;
-    memo = vector<int>(n,-1);
-    
-   int ans= helper(nums,-OO,0);
-   
-   
-    
-    return ans;
-}
+class Solution {
+private:
+    vector<vector<int>> memo;
+    int helper(vector<int>&ar,int idx,int prev){
+        int n = ar.size();
+        if(idx>=n){return 0;}// 1,2,3
+
+        if( memo[idx][prev+1] !=-1){
+            return memo[idx][prev+1];
+        }
+        
+        int pick = 0, leave = 0;
+                
+        if(prev == -1 or ar[idx]>ar[prev])
+            pick = 1 + helper(ar,idx+1,idx);
+        
+        leave = helper(ar,idx+1,prev);
+
+        return memo[idx][prev+1] =  max(pick,leave);
+    }
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        memo = vector<vector<int>>(n,vector<int>(n+1,-1));
+        return helper(nums,0,-1);
+    }
 };
