b7320fcb8b5895482192ec91a4434ca95e439016
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-04-25 09:54:31 +0200

    Time: 66 ms (96.49%), Space: 35.4 MB (92.24%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,26 +1,33 @@
 class SmallestInfiniteSet {
+    set<int> addedIntegers;
+    int currentInteger;
 public:
-    set<int>set;
     SmallestInfiniteSet() {
-       for(int i=1;i<=1000;i++){
-        set.insert(i);
-       } 
+        // The positive integer set's first element will be 1.
+        currentInteger = 1;
     }
-    
     int popSmallest() {
-        auto ans = *set.begin();
-        set.erase(ans);
-        return ans;
+        int answer;
+        // If there are numbers in the sorted-set, 
+        // first element is lowest among all the available numbers.
+        if (!addedIntegers.empty()) {
+            answer = *addedIntegers.begin();
+            addedIntegers.erase(addedIntegers.begin());
+        } 
+        // Otherwise, the smallest number of large positive set 
+        // denoted by 'currentInteger' is the answer.
+        else {
+            answer = currentInteger;
+            currentInteger += 1;
+        }
+        return answer;
     }
-    
     void addBack(int num) {
-        set.insert(num);
+        if (currentInteger <= num || 
+            addedIntegers.find(num) != addedIntegers.end()) {
+            return;
+        }
+        // We push 'num' in the sorted-set if it isn't already present.
+        addedIntegers.insert(num);
     }
 };
-
-/**
- * Your SmallestInfiniteSet object will be instantiated and called as such:
- * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
- * int param_1 = obj->popSmallest();
- * obj->addBack(num);
- */
