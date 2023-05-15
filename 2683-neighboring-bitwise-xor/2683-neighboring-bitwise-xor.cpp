a5a6b6b85d2690b35d9400fd6156b16ff3e3389a
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-05-15 10:34:35 +0300

    Time: 353 ms (12.50%), Space: 259.5 MB (81.25%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
@@ -1,49 +1,19 @@
-
 class Solution {
-private:
-    bool  isEqual(vector<int>&ar1,vector<int>&ar2){
-        for (int i = 0; i < ar1.size(); ++i) {
-            if(ar1[i] != ar2[i])
-                return 0;
-        }
-        return 1;
-    }
-    void xor_ar(vector<int>&ar){
-        int n = ar.size();
-        if(n<=1)return;
-
-        for (int i = 0; i < n-1; ++i) {
-            ar[i] ^= ar[i+1];
-        }
-        ar[n-1] ^=ar[0];
-    }
 public:
     bool doesValidArrayExist(vector<int>& derived) {
         int n = derived.size();
-        if(n==1 && derived[0]==1)
-            return 0;
-        vector<int>original(n);
-
-        int x= 0;//fisrt case
-
-        for (int k = 0; k <=1 ; ++k) {
-            
-            original[0]=k;
-            
-            for (int i = 1; i < n-1; ++i) {
-                original[i] = derived[i-1]^original[i-1];
-            }
-            
-            original[n-1] = original[0]^derived[n-1];
-
-            xor_ar(original);
-            
-            if (isEqual(original,derived))
-                return 1;
+        /**
+         original = [w,x,y,z] 
+         derived = [w^x,x^y,y^z,z^z] , xoring derived from [0 : n-2] ->
+                   -> w^x ^ x^y ^ y^z = w^z , 
+                   if derived from [0 : n-2] == [n-1] return true. 
+         * */
+         int xor_ = 0;
+        for (int i = 0; i < n-1; ++i) {  // loop [0,n-2]
+            xor_ ^= derived[i];
         }
         
-
-        return 0;
+        return xor_ == derived[n-1];
 
     }
 };
