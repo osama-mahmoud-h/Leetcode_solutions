593c42f7452dc667642e4b925cb977817a21aca2
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-08-06 09:32:35 +0300

    Time: 0 ms (100.00%), Space: 6.1 MB (57.83%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        int m = to_string(high).length();
-        vector<int>ans , digits(m,0);
-
-        int j = m-1;
-        int tmp = low;
-        while(j>=0 && tmp>0){
-            digits[j--] = 0;
-            tmp /= 10;
+        string digits = "123456789";
+        vector<int> res;
+        int nl = to_string(low).length();
+        int nh = to_string(high).length();
+        //cout<<nl<<" "<<nh<<"\n";
+        for(int i = nl; i <= nh; i++){
+            for(int j = 0; j < 10 - i; j++){
+                int num = stoi(digits.substr(j, i));
+                //cout<<num<<" " ;
+                if(num >= low && num <= high) res.push_back(num);
+            }
         }
-        j++;
-
-        while (j>=0){
-                for (int x = digits[j]+1; x < 9 ; ++x) {
-                    long long int  sum = 0;
-                    for (int i = j; i < m ; ++i) {
-                        digits[i] = i>j ? digits[i-1]+1 : x;
-                        if(digits[i]>9){
-                            sum = 0;
-                            break;
-                        }
-                        sum += digits[i]*(pow(10,m-i-1));
-                    }
-                    if(sum>=low && sum<=high)
-                        ans.push_back(sum);
-                }
-            j--;
-        }
-        return ans;
+        return res;
     }
 };
