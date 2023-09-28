76e89f38a2de4be53ce28c4821a62c1327bf5d08
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-09-28 07:37:28 +0300

    Time: 10 ms (32.57%), Space: 16.6 MB (25.63%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,24 +1,11 @@
 class Solution {
-    void swap(vector<int>&ar,int from,int to){
-        int temp = ar[from];
-        ar[from]=ar[to];
-        ar[to]=temp;
-    }
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int i=0,j=0, n=nums.size();
-        
-        while(i<n&&(nums[i]%2==0))i++;
-        
-        j=i+1;
-        while(j<n){
-            if(nums[j]%2==0){
-                swap(nums,i,j);
-                i++;
-            }
-            j++;
+        int i=0,j=-1, n=nums.size();
+        while(++j<n){
+            if(nums[j]%2==0)
+                swap(nums[i++],nums[j]);
         }
         return nums;
-        
     }
 };
