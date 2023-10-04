bfd2cc268f5cd68be0538cb80a6184fc03cb5417
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-10-04 08:49:34 +0300

    Time: 129 ms (28.92%), Space: 63.4 MB (46.91%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,57 @@
+
+class MyHashMap {
+private:
+    int size = 10;
+    double loadFactor = 0.75;
+    vector<list<pair<int,int>>>hMap;
+    int curSize;
+    int hash(int n){
+        return n % this->size;
+    }
+    void resize(){
+        size*=2;
+        vector<list<pair<int,int>>>temp(this->size);
+        for (const auto &chain: hMap){
+            for (const auto &item: chain){
+                int k = item.first;
+                int val = item.second;
+                temp[hash(k)].push_back({k,val});
+            }
+        }
+        hMap = temp;
+    }
+    bool loadFactorExceeded(){
+        return (double)this->curSize / this->size >= this->loadFactor;
+    };
+public:
+    MyHashMap() {
+        hMap.resize(10);
+        curSize = 0;
+    }
+
+    void put(int key, int value) {
+        if(loadFactorExceeded()){
+            this->resize();
+        }
+        remove(key);
+        hMap[hash(key)].push_back({key,value});
+        curSize++;
+    }
+
+    int get(int key) {
+        for (const auto &item: hMap[hash(key)]){
+            if(item.first==key){
+                return item.second;
+            }
+        }
+        return -1;
+    }
+
+    void remove(int key) {
+         int val = this->get(key);
+        if(val!=-1){
+            hMap[hash(key)].remove({key, val});
+            this->curSize--;
+        }
+    }
+};
