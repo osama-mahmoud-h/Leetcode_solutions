3023b44cf6ca17417fac462f5194831069993d61
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2023-12-03 08:38:44 +0200

    Time: 4 ms (78.97%), Space: 10.8 MB (5.92%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    vector<int> substractPoints(vector<int>p1,vector<int>p2) {
+    vector<int> substractPoints(vector<int>&p1,vector<int>&p2) {
         return {p1[0] - p2[0], p1[1] - p2[1]};
     }
 
@@ -8,22 +8,10 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int n = points.size();
         int ans = 0;
-        // sort(points.begin(),points.end(),([&](vector<int>&a,vector<int>&b){
-        //     if(a[0]==b[0])
-        //         return a[1]<b[1];//if the x coordinates are equal then sort by y coordinates
-        //     return a[0]<b[0]; // else sort by x coordinates
-        // }));
-    
 
         for (int i = 0; i < n-1; ++i) {
             vector<int>pDiff = substractPoints(points[i],points[i+1]);
-            if(pDiff[0]==0 || pDiff[1]==0){
-                ans += max(abs(pDiff[0]),abs(pDiff[1]));
-            } else if(abs(pDiff[0])==abs(pDiff[1])){
-                ans += abs(pDiff[0]);
-            } else{
-                ans += min(abs(pDiff[0]),abs(pDiff[1])) + abs(abs(pDiff[0])-abs(pDiff[1]));
-            }
+            ans += max(abs(pDiff[0]),abs(pDiff[1]));
         }
         return ans;
     }
