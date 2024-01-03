b07c9c2db628c0e35eb9a7ac721fb5ae37f43736
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2024-01-03 07:27:28 +0200

    Time: 174 ms (6.29%), Space: 23.3 MB (82.87%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int sum_bits(string &s){
+        int ans = 0;
+        for (const auto &item: s){
+            ans += item-'0';
+        }
+        return ans;
+    }
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int ans = 0, prev = 0;
+
+        for (const auto &item: bank){
+           int sum = sum_bits(const_cast<string &>(item));
+            if(sum){
+                if(prev){
+                    ans += prev * sum;
+                }
+                prev = sum;
+            }
+        }
+        return ans;
+    }
+};
