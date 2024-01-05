be6ee7c5b80d40be9b452488876f4ae15f631747
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2024-01-05 14:26:59 +0200

    Time: 920 ms (9.40%), Space: 292.7 MB (6.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,28 +1,27 @@
-
 class Solution {
 private:
-    vector<vector<int>> memo;
+    int INF = 1e9;
+    vector<vector<int>>memo;
     int helper(vector<int>&ar,int idx,int prev){
         int n = ar.size();
         if(idx>=n){return 0;}// 1,2,3
+        //string key = to_string(idx)+"."+ to_string(prev);
 
-        if( memo[idx][prev+1] !=-1){
+        if(memo[idx][prev+1]!=-1){
             return memo[idx][prev+1];
         }
-        
+        int prev_val = prev == -1 ? -INF : ar[prev];
         int pick = 0, leave = 0;
-                
-        if(prev == -1 or ar[idx]>ar[prev])
+        if(ar[idx]>prev_val)
             pick = 1 + helper(ar,idx+1,idx);
-        
+
         leave = helper(ar,idx+1,prev);
 
         return memo[idx][prev+1] =  max(pick,leave);
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        memo = vector<vector<int>>(n,vector<int>(n+1,-1));
+memo = vector<vector<int>>(nums.size(),vector<int>(nums.size()+1,-1));
         return helper(nums,0,-1);
     }
 };
