b68a5b7d5e0f0ef5b1371f5e457a4142aed14b3c
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2024-03-13 09:28:41 +0200

    Time: 3 ms (51.39%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,28 +1,9 @@
 class Solution {
-public:
-    int pivotInteger(int n) {
-        if(n==1)return n;
-        
-        vector<int>prefix_sum(n,0);
-        vector<int>sufix_sum(n,0);
-        
-        int  j = n-1 ;
-
-        prefix_sum[0] = 1;
-        sufix_sum[j] = n;
-        
-        for(int x = 1, y=n-2; x<n; x++,y--){
-            prefix_sum[x] = (x+2)*(x+1)/2;
-            sufix_sum[y] = sufix_sum[y+1]+(y+1);
-        }
-        
-        for(int i=0; i<n;i++){
-            if(prefix_sum[i]==sufix_sum[i]){
-                return ++i;
-            }
-        }
-        
-        
-        return -1;
-    }
+ public:
+  int pivotInteger(int n) {
+        const int sum = (n * (n + 1) / 2);
+        const int pivot = sqrt(sum);
+        // If pivot * pivot is equal to sum (pivot found) return pivot, else return -1
+        return pivot * pivot == sum ? pivot : -1;
+  }
 };
