365eee9b4e6bf8576fafa2ffb81dd2ca72c82b42
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2024-06-12 18:45:24 +0300

    Time: 0 ms (100.00%), Space: 9.9 MB (34.14%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = nums.size() - 1, k = 0;
+
+        while (k <= j) {
+            if (nums[k] == 0) {
+                swap(nums[i], nums[k]);
+                i++;
+                k++;
+            } else if (nums[k] == 2) {
+                swap(nums[k], nums[j]);
+                j--;
+            } else {
+                k++;
+            }
+        }
+    }
+};
