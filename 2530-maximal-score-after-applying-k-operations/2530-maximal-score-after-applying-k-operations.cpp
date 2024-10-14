42c6e549c34e77cfabebf17f07c9be2a108a80db
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2024-10-14 09:06:56 +0300

    Time: 192 ms (66.58%), Space: 79.6 MB (64.84%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>>pq;
+        priority_queue<int>pq;
         
         long long ans = 0;
-        int j = 0;
         for(const auto &i: nums){
-            pq.push({i,j++});
+            pq.push(i);
         }
         
         while(k--){
             auto top = pq.top();
             pq.pop();
-            ans += top.first;
-            pq.push({ceil(top.first/3.0), top.second});
-            nums[top.second] = ceil(top.first/3.0);
+            ans += top;
+            pq.push(ceil(top/3.0));
         }
         return ans;
     }
