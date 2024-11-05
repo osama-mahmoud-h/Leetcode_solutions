f0b798ed5c2e649c683190c522f835282504d0a7
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2024-11-05 09:55:03 +0200

    Time: 0 ms (100.00%), Space: 12.1 MB (67.52%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int ans = 0;
-        int n = s.length(), i = 0, j = 0, prev_digit_sum = 0;
+        int minChangesRequired = 0;
 
-        while (i<n){
-            char cur = s[i];
-            int sum_digits = 0;
-            while (i<n and cur==s[i]){
-                i++;
-                sum_digits++;
+        for (int i = 0; i < s.length(); i += 2) {
+            if (s[i] != s[i + 1]) {
+                minChangesRequired++;
             }
-            if(j>0){
-                if(prev_digit_sum%2==1){
-                    sum_digits--;
-                    ans++;
-                }
-            }
-            j++;
-            prev_digit_sum = sum_digits;
         }
-        return ans;
+
+        return minChangesRequired;
     }
 };
