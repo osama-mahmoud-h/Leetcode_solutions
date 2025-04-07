0f8f5a05a5d61c777f592d39569443826d0b11e5
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-04-07 11:38:17 +0200

    Time: 420 ms (5%), Space: 346.9 MB (5.25%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,28 @@
 class Solution {
 private:
+    vector<vector<int>>memo;
     bool helper(vector<int>&A, int idx, int sum ,int rem){
         if(idx >= A.size()){
-            return false;
+            return 0;
         }
         sum += A[idx];
         rem -= A[idx];
         if(sum==rem)
-            return true;
+            return memo[idx][sum] =  1;
+
+        if(memo[idx][sum]!=-1){
+            return memo[idx][sum];
+        }
         bool pick = helper(A,idx+1, sum, rem);
         bool leave = helper(A,idx+1, sum -  A[idx], rem +  A[idx]);
-        return pick or leave;
+
+        return memo[idx][sum] = (pick or leave);
     }
 public:
     bool canPartition(vector<int>& A) {
         sort(A.begin(), A.end());
         int sum = accumulate(A.begin(), A.end(),0);
+        memo = vector<vector<int>>(A.size(),vector<int>(20001,-1));
         return helper(A,1,0,sum);
     }
 };
