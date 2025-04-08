0dc90546e58092c9d367f05b23ba8f3d0b080a39
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-04-08 09:54:20 +0200

    Time: 8 ms (27.3%), Space: 27.9 MB (43.34%) - LeetHub

---
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int>seen;
+        int n = (int)nums.size();
+        
+        for (int i = n - 1; i >= 0 ; --i) {
+            if(seen.find(nums[i]) != seen.end()){
+                cout<<i<<"\n";
+                return ceil((i+1)/3.0) ;
+            }
+            seen.insert(nums[i]);
+        }
+        return 0;
+    }
+};
