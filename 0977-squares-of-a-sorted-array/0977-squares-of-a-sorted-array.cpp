ddd9fa857667bc48abc58045a1a402bc38c2af18
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-03 11:34:27 +0300

    Time: 3 ms (55.3%), Space: 30.2 MB (85.33%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -2,31 +2,24 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
+        vector<int> result(n);
         
-        int pivot = 0;
-        for (int i = 0; i < n-1; ++i) {
-            nums[i] = nums[i] * nums[i];
-            if(nums[i] > nums[i+1]*nums[i+1]){
-                pivot = i+1;
+        int left = 0, right = n - 1;
+        int pos = n - 1;
+        
+        while (left <= right) {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+            
+            if (leftSq > rightSq) {
+                result[pos--] = leftSq;
+                ++left;
+            } else {
+                result[pos--] = rightSq;
+                --right;
             }
         }
-        nums[n-1] = nums[n-1] * nums[n-1];
-       // cout<<pivot;
-        int i = pivot-1, j = pivot;
-        vector<int>ans ;
-
-        if(pivot==n-1){
-            reverse(nums.begin(), nums.end());
-            return nums;
-        }
         
-        while(j<n or i>0){
-            if(i >=0 and nums[i] < nums[j]){
-                ans.push_back(nums[i--]);
-            }else if(j<n){
-                ans.push_back(nums[j++]);
-            }
-        }
-        return ans;
+        return result;
     }
 };
