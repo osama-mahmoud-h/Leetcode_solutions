8dd48eeba1f5cdeb723c8084609c5c534466f58e
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-03 11:32:43 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,33 +1,31 @@
-
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-        int i = 0;
-        while(i<n && nums[i]<0){
-            i++;
+        
+        int pivot = 0;
+        for (int i = 0; i < n-1; ++i) {
+            nums[i] = nums[i] * nums[i];
+            if(nums[i] > nums[i+1]*nums[i+1]){
+                pivot = i+1;
+            }
         }
-        int j = i-1;
-        vector<int>ans;
-       // ans.push_back(nums[i]*nums[i]);
-        while (i<n || j>-1){
-           if(i<n){
-               if(j>-1){
-                   if(nums[i]*nums[i]<nums[j]*nums[j]) {
-                       ans.push_back(nums[i] * nums[i]);
-                       i++;
-                   } else{
-                       ans.push_back(nums[j]*nums[j]);
-                       j--;
-                   }
-               }else{
-                   ans.push_back(nums[i]*nums[i]);
-                   i++;
-               }
-           }else{
-               ans.push_back(nums[j]*nums[j]);
-               j--;
-           }
+        nums[n-1] = nums[n-1] * nums[n-1];
+       // cout<<pivot;
+        int i = pivot-1, j = pivot;
+        vector<int>ans ;
+
+        if(pivot==n-1){
+            reverse(nums.begin(), nums.end());
+            return nums;
+        }
+        
+        while(j<n or i>0){
+            if(i >=0 and nums[i] < nums[j]){
+                ans.push_back(nums[i--]);
+            }else if(j<n){
+                ans.push_back(nums[j++]);
+            }
         }
         return ans;
     }
