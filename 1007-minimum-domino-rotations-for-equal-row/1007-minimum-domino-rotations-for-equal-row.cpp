d70d1fd84596fa85b4b3af19d11843bcc801d3ff
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-03 11:09:36 +0300

    Time: 0 ms (100%), Space: 115.5 MB (48.71%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -26,13 +26,7 @@ public:
             min_flips(tops, bottoms, first_bottom),
             min_flips(bottoms,tops ,first_bottom)
         };
-       
-        //cout<<"min_top_flips: "<<min_top_flips<<"\n";
-       // cout<<"min_bottom_flips: "<<min_bottom_flips<<"\n";
 
-        if(ans[0]==-1 and ans[1]==-1 and ans[2]==-1 and ans[3]==-1){
-            return -1;
-        }
 
         int res = INT_MAX;
         for (auto const &i: ans){
@@ -41,6 +35,6 @@ public:
                 res = min(res,i);
         }
         
-        return res;
+        return res==INT_MAX ? -1 : res;
     }
 };
