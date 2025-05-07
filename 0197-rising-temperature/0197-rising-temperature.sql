f0c1c7d27dfba3160ac3994e813bd011e198dd61
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-07 16:04:30 +0300

    Time: 361 ms (24.52%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,16 +1,5 @@
-# Write your MySQL query statement below
-/*
-
-
-*/
-/*
-select DATEDIFF(recordDate , "2015-01-03") as dif 
-from Weather*/
-
-select w1.id 
-from Weather w1
-join Weather w2 
-on w1.temperature > w2.temperature
-and DATEDIFF(w1.recordDate,w2.recordDate) =1
-
-                     
+-- Write your PostgreSQL query statement below
+SELECT w.id
+FROM Weather w
+WHERE 
+w.temperature > (select w2.temperature from  Weather w2 where w2.recordDate = (w.recordDate - INTERVAL '1 day'));
