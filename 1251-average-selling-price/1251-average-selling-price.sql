b85d2b163f03a854e5018da2551f295138e1fb45
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-08 18:13:35 +0300

    Time: 230 ms (48.23%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -2,7 +2,10 @@
 select 
 --*
 p.product_id ,
-round( sum(u.units * p.price) / sum(u.units )::numeric ,2 )as average_price
+ROUND(
+    sum(CASE WHEN u.units IS NULL THEN 0 ELSE u.units * p.price END) / 
+    GREATEST(1,sum(CASE WHEN u.units IS NULL THEN 0 ELSE u.units END ))::numeric  -- max(1,sum)--to avoid /0
+, 2)as average_price
 from Prices p left join UnitsSold u on p.product_id = u.product_id
     and u.purchase_date BETWEEN p.start_date AND p.end_date 
 group by p.product_id;
