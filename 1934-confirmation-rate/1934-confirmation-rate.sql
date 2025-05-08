41c4e9847c86788c79fc6de1a5b9a9d32f6dc735
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-08 15:43:42 +0300

    Time: 241 ms (76.04%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,9 +1,9 @@
 -- Write your PostgreSQL query statement below
 select 
 s.user_id,
-(
+ROUND(
 count(CASE WHEN c.action='confirmed' THEN 1 END) / GREATEST(1,count(c.action))::numeric
-)as confirmation_rate
+, 2)as confirmation_rate
 FROM
  Signups s LEFT JOIN Confirmations c
     ON s.user_id = c.user_id
