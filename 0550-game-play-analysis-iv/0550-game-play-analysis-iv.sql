990f842cdd5c888477340330dc7f32bfe1d69a02
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-11 14:11:10 +0300

    Time: 1063 ms (10.05%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,11 +1,10 @@
 -- Write your PostgreSQL query statement below
 select 
-ROUND(
-  count(case when a.player_id is not null then 1.0 else 0.0 end)::NUMERIC
-    /(select count(DISTINCT a3.player_id) from Activity a3)
-    , 2) as fraction
+    ROUND(
+        count(DISTINCT player_id)::NUMERIC /(select count(DISTINCT a3.player_id) from Activity a3)
+    , 2)
+as fraction
 from Activity a
-
 where (player_id, event_date) in (
     select 
         a2.player_id,
