1390ae7458a13a2602a978612d7818b6d314f2e7
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-11 22:49:18 +0300

    Time: 606 ms (33.36%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,5 +1,11 @@
+WITH count_products_cte AS(
+    SELECT
+        COUNT(*)
+    FROM Product
+)
+
 SELECT
 customer_id
 FROM Customer c
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
+HAVING COUNT(DISTINCT product_key) = (select * from count_products_cte)
