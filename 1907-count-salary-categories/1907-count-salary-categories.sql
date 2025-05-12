5f1cbd5768db29349ad9eb485e34c1326658f674
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-05-12 14:45:14 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,25 +1,9 @@
--- Write your PostgreSQL query statement below
-(
-SELECT 
-    'Low Salary' AS category,
+SELECT
+    CASE
+        WHEN income < 20000 THEN 'Low Salary'
+        WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+        ELSE 'High Salary'
+    END AS category,
     COUNT(*) AS accounts_count
 FROM Accounts
-WHERE income < 20000
-)
-
-    UNION ALL
-(
-SELECT 
-    'Average Salary' AS category,
-    COUNT(*) AS accounts_count
-FROM Accounts
-WHERE income BETWEEN 20000 AND 50000
-)
-    UNION ALL
-(
-SELECT 
-    'High Salary' AS category,
-    COUNT(*) AS accounts_count
-FROM Accounts
-WHERE income > 50000
-)
+GROUP BY category;
