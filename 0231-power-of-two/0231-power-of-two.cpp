b34730ae1d598833d6a63e921b871d20a9ee52b2
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-08-09 19:01:31 +0300

    Time: 0 ms (100%), Space: 8 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-         long long int N = (long long int) n;
+        long long int N = (long long int) n;
         return (N&(N-1))==0 &&n!=0 ;
     }
 };
