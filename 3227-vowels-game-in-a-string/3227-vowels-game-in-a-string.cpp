cd728fddc1c1a6acec073e260583ae5f096ec10a
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-09-12 09:29:56 +0300

    Time: 1294 ms (5.04%), Space: 467.2 MB (5.41%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -7,12 +7,9 @@ public:
     bool doesAliceWin(string s) {
         int cntv = 0;
         for(auto const& c : s){
-            cntv += is_vowel(c);
-        }cout<<cntv;
-
-        if(!cntv) return 0;
-        if(cntv%2 !=0 ) return 1;
-
-        return 1;
+            if(is_vowel(c))
+                return 1;
+        }
+        return 0;
     }
 };
