dc0152b920006d39eac31e1f2b54df22b528ad38
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-10-16 08:41:50 +0300

    Time: 4 ms (96.91%), Space: 40.3 MB (93.69%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,44 +1,32 @@
 class Solution {
-private:
-    int longest_increasing_from(vector<int>& nums, int idx){
+
+     int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size();
-        int sum = 1;
-        int mx = 1;
-        for (int i = idx; i< n-1; ++i) {
-            if(nums[i] < nums[i+1]){
-                mx = max(mx, ++sum);
-            }else{
-                return mx;
+        if (n == 0) return 0;
+
+        int prevLen = 0, currLen = 1, ans = 1;
+
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > nums[i - 1]) {
+                currLen++;
+            } else {
+                if (prevLen > 0)
+                    ans = max(ans, min(prevLen, currLen));
+                ans = max(ans, currLen / 2);
+                prevLen = currLen;
+                currLen = 1;
             }
         }
-       //  cout<<"inc: "<<mx<<"\n";
-        return mx;
-    }
-    int longest_decreasing_from(vector<int>& nums, int idx){
-        int n = nums.size();
-        int sum = 1;
-        int mx = 1;
-        for (int i = idx; i>=1 ; --i) {
-            if(nums[i] > nums[i-1]){
-                mx = max(mx, ++sum);
-            }else{
-                return mx;
-            }
-        }
-       // cout<<"dec: "<<mx<<"\n";
-        return mx;
+
+        // Handle the last segment
+        if (prevLen > 0)
+            ans = max(ans, min(prevLen, currLen));
+        ans = max(ans, currLen / 2);
+
+        return ans;
     }
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        int cnt = 1, mx = 1;
-        int n = nums.size();
-        int cnt_mx = 0;
-        for (int i = k-1; i < n-k; ++i) {
-            cout<<"i: "<<i<<"\n";
-           if(longest_increasing_from(nums, i+1) >= k 
-           and longest_decreasing_from(nums, i) >=k)
-               return true;
-        }
-        return false;
+        return maxIncreasingSubarrays(nums) >= k;
     }
 };
