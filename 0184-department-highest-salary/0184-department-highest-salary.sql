ac3d5b8ecda6c4bdd3bb795cb0c6645c809d75d4
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-12-01 16:57:50 +0200

    Time: 234 ms (59.63%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT 
+        *,
+        RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+    FROM Employee
+) e
+JOIN Department d ON e.departmentId = d.id
+WHERE e.salary_rank = 1
+ORDER BY d.name, e.salary DESC;
