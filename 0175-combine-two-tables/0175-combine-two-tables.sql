5efd48097fc0d882b8bf0f9c4e09bbff7ea04439
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-12-01 15:53:13 +0200

    Time: 235 ms (85.14%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select p.firstName ,p.lastName,a.city,a.state
-from Person p left join Address a
-on p.personId = a.personId
+-- Write your PostgreSQL query statement below
+ select firstName , lastName , a.city , a.state
+from Person p 
+left join Address a on p.personId  = a.personId 
