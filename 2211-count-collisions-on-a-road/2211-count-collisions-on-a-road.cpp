5da3acdafc29b3992d071367b663059160f4b921
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-12-04 06:47:35 +0200

    Time: 2 ms (97.1%), Space: 19.3 MB (50.72%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,33 +1,24 @@
-class Solution {   
+class Solution {
 public:
     int countCollisions(string directions) {
-        stack<char> st;
-        int ans = 0;
-        for (const auto &dir: directions){
-            if(st.empty() or st.top() == 'L'){
-                st.push(dir);
-                continue;
-            }
-            if(st.top()=='R'){
-               if(dir == 'L'){
-                   ans +=2;
-                   st.pop();
-                   st.push('S');
-               }else if(dir == 'S'){
-                   ans++;
-                st.push('S');
-               }else{
-                st.push(dir);
-               }
-            }else{ // is s
-                if(dir == 'L'){
-                    ans++;
-                    st.push('S');
-                }else{
-                    st.push(dir);
-                }
-            }
+        int n = directions.size();
+
+        // skip all leading L's (no collision)
+        int i = 0;
+        while (i < n && directions[i] == 'L') i++;
+
+        // skip all trailing R's (no collision)
+        int j = n - 1;
+        while (j >= 0 && directions[j] == 'R') j--;
+
+        int collisions = 0;
+
+        // count all moving cars in the middle region
+        for (int k = i; k <= j; k++) {
+            if (directions[k] != 'S')
+                collisions++;
         }
-        return ans;
+
+        return collisions;
     }
 };
