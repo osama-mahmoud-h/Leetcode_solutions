7fed7ef01c17c25fb1647e66197e4b1c4f8af4a8
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2025-12-12 09:18:03 +0200

    Time: 2 ms (63.8%), Space: 7.7 MB (91.28%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        high += (high%2);
-        low -= (low%2);
-        return (high - low)/2;
+        return ((high + (high%2)) - (low-(low%2))) / 2;
     }
 };
