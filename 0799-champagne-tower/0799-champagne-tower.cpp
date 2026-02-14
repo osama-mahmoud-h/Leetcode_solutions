230f30a597dc440d2df27c70e252834354d44bfb
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-02-14 06:51:58 +0200

    Time: 10 ms (27.12%), Space: 18.3 MB (35.59%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,13 +1,4 @@
 class Solution {
-    void printMatrix(const vector<vector<double>>& rows) {
-        for (const auto& row : rows) {
-            for (double value : row) {
-                cout << value << " ";
-            }
-            cout << endl;
-        }
-    }
-
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
         vector<vector<double>> rows;
@@ -16,9 +7,7 @@ public:
             vector<double> row(i + 1, 0);
             for (int j = 0; j < i + 1; ++j) {
                 if (j == 0) {
-                    // cout << "j==0 , i: " << i << " , val: " << rows[i - 1][0]
-                    //      << ", poured from up " << (rows[i - 1][0] > 1.0)
-                    //      << " ,with " << (rows[i - 1][0] / 2.0 - 1.0) << "\n";
+                 
                     row[j] = rows[i - 1][0] > 1.0 ? (rows[i - 1][0] - 1.0) / 2.0
                                                   : 0.0;
                 } else if (j == i) {
@@ -39,7 +28,6 @@ public:
             }
             rows.push_back(row);
         }
-        printMatrix(rows);
         return min(1.0, rows[query_row][query_glass]);
     }
 };
