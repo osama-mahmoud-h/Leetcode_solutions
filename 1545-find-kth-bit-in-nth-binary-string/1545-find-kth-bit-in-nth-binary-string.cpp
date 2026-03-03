7ccae4684133ed985cf0d1eefedcd9f6c0b6096a
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-03-03 20:28:19 +0200

    Time: 0 ms (100%), Space: 7.9 MB (63.07%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,23 +1,15 @@
 class Solution {
-    string invert(string s){
-        int n = s.length();
-        for (int i = 0; i < n; ++i){
-            s[i] = !(s[i]-'0') + '0';
-        }
-        return s;
-    }
-    string reverse(string s){
-        std::reverse(s.begin(), s.end());
-        return s;
-    }
 public:
     char findKthBit(int n, int k) {
-        string base = "0";
-        for (int i = 1; i < n; ++i){
-            //cout<<"invert(reverse(base)): "<<invert(reverse(base))<<"\n";
-            base = base + "1" + invert(reverse(base));
-        }
-        //cout<<"base: "<<base<<"\n";
-        return base[k-1];
+        if (n == 1) return '0';
+        
+        int len = (1 << n) - 1;
+        int mid = (len + 1) / 2;
+        
+        if (k == mid) return '1';
+        if (k < mid) return findKthBit(n - 1, k);
+        
+        char c = findKthBit(n - 1, len - k + 1);
+        return c == '0' ? '1' : '0';
     }
 };
