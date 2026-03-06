aa22630d57f681da61debe1b3074f14923264572
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-03-06 09:04:48 +0200

    Time: 0 ms (100%), Space: 8.1 MB (77.26%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int i = 0, n = s.length();
-        int single = 0, seq = 0;
-        while(i < n){
-            int sum = 0;
-            while(i < n and s[i] == '1'){
-                sum++, i++;
-            }
-            single += sum == 1 ? 1 : 0;
-            seq += sum > 1 ? 1 : 0;
-            i += sum == 0 ? 1 : 0;
-        }
-        //cout<<"seq: "<<seq<<" , single: "<<single<<"\n";
-        return (seq == 1 and single == 0) || (single == 1 and seq == 0);
+        return s.find("01") == string::npos;
     }
 };
