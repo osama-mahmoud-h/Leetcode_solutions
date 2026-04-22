005392362d3d7bec1fc4fb580af3b06155005811
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-04-22 07:16:04 +0200

    Time: 10 ms (38.13%), Space: 18.4 MB (5.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,33 +1,22 @@
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       
+        unordered_map<int,list<int>> num_idx;
         int n = nums.size();
+        for(int i = 0; i<n; i++){
+            num_idx[nums[i]].push_back(i);
+        }
+
+        sort(nums.begin(), nums.end());
         int l = 0, h = n-1;
-        vector<int>ans(2,-1);
-
-        unordered_map<int,vector<int>> map;
-
-       for(int i= 0; i < n; i++){
-            map[nums[i]].push_back(i);
-        }
-         sort(nums.begin(), nums.end());
-
-
-        while(l<h){
-            if(nums[l] + nums[h] > target){
+        while(l < h){
+            if(nums[l]+nums[h] > target)
                 h--;
-            }else if(nums[l] + nums[h] < target){
+            else if (nums[l]+nums[h] < target)
                 l++;
-            }else{
-                int low_idx = *map[nums[l]].begin();
-                int high_idx = *(--map[nums[h]].end());
-                return vector<int>{low_idx, high_idx};
-            }
+            else break;    
         }
 
-        return ans;
-
+        return vector<int>{num_idx[nums[l]].front(), num_idx[nums[h]].back()}; 
     }
 };
