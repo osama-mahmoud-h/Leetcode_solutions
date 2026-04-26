205a75131799ca2e898d367a282e5331ff277e8b
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-04-26 08:21:18 +0300

    Time: 25 ms (67.59%), Space: 65 MB (75.39%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,55 +1,42 @@
 class Solution {
-public:
-    vector<vector<int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
-    bool vis[501][501];
-    
-    bool dfs(vector<vector<char>>& ar,char curr,pair<int,int>idx,pair<int,int>parent){
-          
-    if(idx.first<0||idx.first>=ar.size()||idx.second<0||idx.second>=ar[0].size()
-       ||ar[idx.first][idx.second]!=curr){
-        return false;
-    }
-      
-    if(vis[idx.first][idx.second]){
-        return 1;
-    }
-       
-        
-        vis[idx.first][idx.second]=1;
-    for(auto p:dir){
-        if((p[0]+idx.first == parent.first)&&((p[1]+idx.second == parent.second))){
-            //do nothing
-        } else{
-                     
+private:
+    vector<vector<int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    bool has_cycle(int i, int j,int pi, int pj, vector<vector<char>>& grid,
+                   vector<vector<bool>>& visited) {
 
-            if(dfs(ar,curr,{p[0]+idx.first,p[1]+idx.second},{idx.first,idx.second})){
+        int n = grid.size(), m = grid[0].size();
+        visited[i][j] = 1;
+        for (const auto& dir : dirs) {
+            int x = dir[0] + i, y = dir[1] + j;
+            if (x < 0 or x >= n or y < 0 or y >= m or grid[x][y] != grid[i][j])
+                continue;
+
+            if (!visited[x][y]) {
+                if (has_cycle(x, y, i, j, grid, visited))
+                    return 1;
+            }
+            else if (x != pi or y != pj) { // avoid parent cycly
                 return 1;
             }
+
         }
-    }
-        
         return 0;
-}
-bool containsCycle(vector<vector<char>>& grid) {
-    int n=grid.size();
-    
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <grid[i].size() ; ++j) {
-    vis[i][j]=0;
-        }
     }
-            
-            
-            
-            for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <grid[i].size() ; ++j) {
-            if(!vis[i][j])
-            if(dfs(grid,grid[i][j],make_pair(i,j),make_pair(-1,-1)))
-                return 1;
-            //cout<<"----------\n";
+
+public:
+    bool containsCycle(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<bool>> visited(n, vector<bool>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visited[i][j])
+                    continue;
+                if (has_cycle(i, j,-1,-1, grid, visited)) {
+                    //cout << i << "  , " << j << "\n";
+                    return 1;
+                }
+            }
         }
-        
+        return 0;
     }
-    return 0;
-}
 };
