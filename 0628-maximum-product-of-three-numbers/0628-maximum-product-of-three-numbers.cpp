4cd61ee2358a756bbe20c5390b630e860dc179ee
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-07-26 12:36:30 +0300

    Time: 22 ms (8.84%), Space: 34.9 MB (1.76%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -5,8 +5,9 @@ public:
         int n = nums.size();
         return max({ 
             nums[0] *  nums[1] * nums[2],
-            nums[0] * nums[n-1] * nums[n-2],
-            nums[0] * nums[1] * nums[n-1]
+            nums[0] * nums[n-1] * nums[n-2]
+            //,
+           // nums[0] * nums[1] * nums[n-1]
         });
     }
 };
