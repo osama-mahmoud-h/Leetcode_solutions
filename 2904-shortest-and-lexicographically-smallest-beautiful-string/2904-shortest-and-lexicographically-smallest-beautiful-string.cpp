0c6a06071105f26f8171ef67e6075e7f01bbfa81
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-08-26 21:32:09 +0300

    Time: 0 ms (100%), Space: 8.6 MB (82.87%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -9,18 +9,19 @@ public:
             if(s[h] == '1'){
                 sum ++;
                 //cout<<"cur_window: "<<s.substr(l, h - l + 1)<<" , sum: "<<sum<<"\n";
-                if(sum == k){ // compare with existing
-                    string cur_window = s.substr(l, h - l + 1);
+                // if(sum == k){ // compare with existing
+                //     string cur_window = s.substr(l, h - l + 1);
 
-                    ans = ans.empty() ? cur_window : (cur_window.length() != ans.length() ? (cur_window.length() < ans.length() ? cur_window : ans) : (cur_window < ans ? cur_window : ans));
-                }else if(sum >= k){                  
-                    sum--;
-                    while (++l < h && s[l] != '1' );
+                //     ans = ans.empty() ? cur_window : (cur_window.length() != ans.length() ? (cur_window.length() < ans.length() ? cur_window : ans) : (cur_window < ans ? cur_window : ans));
+                // }else 
+                if(sum >= k){                  
+ 
                      
                     string cur_window = s.substr(l, h - l + 1);
                     //cout<<" , sum: "<<sum<<", l: "<<l<<", after shrink: "<<cur_window<<"\n";
                     ans = ans.empty() ? cur_window : (cur_window.length() != ans.length() ? (cur_window.length() < ans.length() ? cur_window : ans) : (cur_window < ans ? cur_window : ans));
-
+                    sum--;
+                    while (++l < h && s[l] != '1' );
                     // h--;
                 }
             }
