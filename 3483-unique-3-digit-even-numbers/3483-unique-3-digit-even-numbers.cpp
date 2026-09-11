17ed16c685b438f144fdf93a7f455d8fccc45999
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-09-11 11:06:48 +0300

    Time: 3 ms (84.06%), Space: 31.3 MB (21.94%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        set<int> evens ;
+        for (int el : digits) {
+            if (el % 2 == 0) evens.insert(el);
+        }
+        int ans = 0;
+        unordered_set<int> uniques;
+        int n = digits.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if(digits[i]%2 == 1)
+                continue;
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    if(digits[k] == 0 || (i==j || j==k || i==k))
+                        continue;
+                    uniques.insert(digits[k]*100 + digits[j]*10 + digits[i]);
+                }
+            }
+        }
+
+        return uniques.size();
+    }
+};
