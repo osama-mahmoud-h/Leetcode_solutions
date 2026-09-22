7e420e0d7112d2c31d99b69498084eb4131c249d
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-09-22 10:56:23 +0300

    Time: 402 ms (86.52%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,11 +1,53 @@
 -- Write your PostgreSQL query statement below
-SELECT 
-TO_CHAR(trans_date, 'YYYY-MM') as month,
-country,
-(COUNT(*)) as trans_count ,
-(COUNT(CASE WHEN t.state = 'approved' THEN 1 END)) as approved_count ,
-(SUM(amount)) as trans_total_amount ,
-(SUM(CASE WHEN t.state='approved' THEN amount ELSE 0 END))  as approved_total_amount
-FROM Transactions t
 
-GROUP BY month, country
+
+select 
+    TO_CHAR(date_trunc('month', trans_date) , 'YYYY-MM') as month,
+    country ,
+    count(*) as trans_count ,
+    count(
+        case t.state 
+        when 'approved'
+        then 1 
+        end
+    )as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(
+        case t.state 
+        when 'approved'
+        then t.amount
+        else 0 
+        end
+    ) as approved_total_amount
+from Transactions t
+group by  date_trunc('month', trans_date), country 
+
+
+;
+
+
+-- SELECT
+--     DATE_TRUNC('month', t.trans_date) AS month,
+--     t.country,
+--     COUNT(*) AS trans_count,
+
+--     COUNT(
+--         CASE
+--             WHEN t.state = 'approved' THEN 1
+--         END
+--     ) AS approved_count,
+
+--     SUM(t.amount) AS trans_total_amount,
+
+--     SUM(
+--         CASE
+--             WHEN t.state = 'approved' THEN t.amount
+--             ELSE 0
+--         END
+--     ) AS approved_total_amount
+
+-- FROM Transactions t
+
+-- GROUP BY
+--     DATE_TRUNC('month', t.trans_date),
+--     t.country;
