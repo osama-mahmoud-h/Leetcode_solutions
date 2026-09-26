6cd515f71e8a7a10c06fb5edbefcc2c8a879aa29
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-09-26 07:56:44 +0300

    Time: 10 ms (18.3%), Space: 44.2 MB (68.99%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] strs = s.trim().split("\\s+");
+        Collections.reverse(Arrays.asList(strs));
+
+        return String.join(" ", strs);
+    }
+}
