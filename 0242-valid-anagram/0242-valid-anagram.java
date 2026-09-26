9846ac6402184c33a98c3318740ccb6dc0575346
Osama Mahmoud <84927237+osama-mahmoud-h@users.noreply.github.com>
2026-09-26 07:10:31 +0300

    Time: 5 ms (63.25%), Space: 46.3 MB (40.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,19 +4,14 @@ class Solution {
         int n = s.length(), m = t.length();
         if(n != m)
             return false;
+        
+        char [] chars = s.toCharArray();
+        char [] chart = t.toCharArray();
 
-        int[] mapS =  new int[26] , mapT = new int[26]; // by default is 0;
-        Arrays.fill(mapS, 0);
-        Arrays.fill(mapT, 0);
+        Arrays.sort(chars); Arrays.sort(chart);
 
-        for (int i = 0; i < n; i++) {
-            mapS[s.charAt(i) - 'a']++;
-            mapT[t.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < 26; i++) {
-            if(mapS[i] != mapT[i])
-                return false;
-        }
-        return true;
+        s = new String(chars); t = new String(chart);
+
+        return s.equals(t);
     }
 }
